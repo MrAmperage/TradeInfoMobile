13c4ace266156140ab5db64bc423f47793ddd8fd
Mr.Amperage <Mr.Amperage@protonmail.com>
2021-02-23 10:59:35 +0700

    Update to 0.0.1

---
+++ app/src/main/java/ru/mr_amperage/tradeinfomobile/LoginActivityClass.java
@@ -0,0 +1,13 @@
+package ru.mr_amperage.tradeinfomobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+public class LoginActivityClass extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity_layout);
+    }
+}
